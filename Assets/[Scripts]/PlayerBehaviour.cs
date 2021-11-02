27e0762c43b9138e419a04d3a31725cf80a9e62f
lucasdunster <60508486+lucasluvia@users.noreply.github.com>
2021-11-02 15:04:18 -0400

    Added Simple Player Control

--- Assets/[Scripts]/PlayerBehaviour.cs
+++ Assets/[Scripts]/PlayerBehaviour.cs
@@ -7,6 +7,10 @@ public class PlayerBehaviour : MonoBehaviour
     [Header("Movement")]
     public float horizontalForce;
     public float verticalForce;
+    public bool isGrounded;
+    public float groundRadius;
+    public LayerMask groundLayerMask;
+    public Transform groundOrigin;
 
     private Rigidbody2D rigidbody;
 
@@ -15,28 +19,48 @@ public class PlayerBehaviour : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         Move();
+        CheckIfGrounded();
     }
 
     private void Move()
     {
-        // Keyboard / Mouse Input
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-        Vector2 worldTouch = new Vector2();
-
-
-        // Touch Input
-        foreach (var touch in Input.touches)
+        if(isGrounded)
         {
-            worldTouch = Camera.main.ScreenToWorldPoint(touch.position);
+            // Keyboard / Mouse Input
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Vertical");
+            float jump = Input.GetAxisRaw("Jump");
+
+            Vector2 worldTouch = new Vector2();
+
+
+            // Touch Input
+            foreach (var touch in Input.touches)
+            {
+                worldTouch = Camera.main.ScreenToWorldPoint(touch.position);
+            }
+
+            var horizontalMoveForce = x * horizontalForce * Time.deltaTime;
+            var jumpMoveForce = jump * verticalForce * Time.deltaTime;
+
+            rigidbody.AddForce(new Vector2(horizontalMoveForce, jumpMoveForce));
+
         }
-
-        Debug.Log("X: " + x);
-        Debug.Log("Y: " + y);
-        Debug.Log("Touch: " + worldTouch);
-
     }
+
+    private void CheckIfGrounded()
+    {
+        RaycastHit2D hit = Physics2D.CircleCast(groundOrigin.position, groundRadius, Vector2.down, groundRadius, groundLayerMask);
+        isGrounded = (hit) ? true : false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundOrigin.position, groundRadius);
+    }
+
 }
